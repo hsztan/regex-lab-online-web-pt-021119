0d871bd7e218e14c4691fff95bcf0e2eece2cb63
Henry Sztanski <henry@sztanski.com>
2019-02-23 01:21:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,11 +9,11 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def valid_phone_number?(phone)
